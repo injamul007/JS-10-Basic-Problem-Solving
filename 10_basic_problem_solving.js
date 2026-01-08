8cedae783906ee22f358b5ba3f58c7a3508d92cc
injamul007 <injamul007hoque@gmail.com>
2026-01-08 09:19:55 +0600

    problem 5 is solved

--- 10_basic_problem_solving.js
+++ 10_basic_problem_solving.js
@@ -58,3 +58,18 @@ function findMaxNumber(arr) {
 }
 
 // findMaxNumber([5, 1, 9, 3])
+
+
+//? Problem 5: Remove Duplicates from an Array
+
+function removeDuplicatesFromArr(arr) {
+  let newArr = []
+  for(let i = 0; i < arr.length; i++) {
+    if(!newArr.includes(arr[i])) {
+      newArr.push(arr[i])
+    }
+  }
+  console.log(newArr)
+}
+
+// removeDuplicatesFromArr([1, 2, 2, 3, 4, 4]);
