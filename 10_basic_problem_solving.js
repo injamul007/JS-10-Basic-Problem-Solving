5f92dfa9ed5d8b426253e3d985d8c9dff6a5fbef
injamul007 <injamul007hoque@gmail.com>
2026-01-08 07:47:23 +0600

    problem 2 is solved

--- 10_basic_problem_solving.js
+++ 10_basic_problem_solving.js
@@ -9,3 +9,21 @@ function reversString(str) {
 const reverseStr = reversString('hello')
 
 // console.log(reverseStr)
+
+
+//? Problem 2: Count Vowels in a String
+
+function countVowels(str) {
+  let count = 0;
+  const allVowel = "AEIOU".toLowerCase();
+  const strLowerCase = str.toLowerCase()
+
+  for(let i = 0; i < strLowerCase.length; i++) {
+    if(allVowel.includes(strLowerCase[i])) {
+      count++
+    }
+  }
+  console.log(count)
+}
+
+// countVowels("programming")
